ed3b137bc003bd16db7e1d7a97f5108650a44507
Danilo Dias <danilolive30@hotmail.com>
2023-12-14 16:54:56 -0300

    feat: refactor for components

---
+++ src/app/components/FormLogin/index.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { v4 as uuidv4 } from "uuid";
+
+const schema = z.object({
+  id: z.string(),
+  email: z
+    .string()
+    .trim()
+    .email({ message: "E-mail inválido" })
+    .min(1, { message: "Obrigatório*" }),
+  pass: z.string().trim().min(1, { message: "Obrigatório*" }),
+});
+
+type FormDataProps = z.infer<typeof schema>;
+
+export const FormLogin = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    mode: "all",
+    criteriaMode: "all",
+    resolver: zodResolver(schema),
+    defaultValues: {
+      id: uuidv4(),
+      email: "",
+      pass: "",
+    },
+  });
+
+  const onSubmitForm = () => {
+    //TODO: Salvar o objeto user no contexto
+  };
+
+  return (
+    <form
+      className="flex flex-col max-h-[25%] p-3 mb-20 gap-3 justify-center items-center"
+      onSubmit={handleSubmit(onSubmitForm)}
+    >
+      <div className="flex flex-col">
+        <label className="font-mono text-black text-xl font-bold mb-3">
+          E-mail
+        </label>
+        <input
+          {...register("email")}
+          type="text"
+          className="w-64 h-14 rounded-sm bg-white text-black shadow-md"
+          maxLength={255}
+        />
+        {errors.email && <p className="text-black">{errors.email.message}</p>}
+      </div>
+      <div className="flex flex-col">
+        <label className="font-mono text-black text-xl font-bold mb-3">
+          Senha
+        </label>
+        <input
+          {...register("pass")}
+          type="password"
+          className="w-64 h-14 rounded-sm bg-white text-black shadow-md"
+          maxLength={64}
+        />
+        {errors.pass && <p className="text-black">{errors.pass.message}</p>}
+      </div>
+      <div>
+        <button
+          className="w-56 h-14 
+        rounded-xl bg-black shadow-md hover:bg-slate-500 disabled:cursor-not-allowed"
+          disabled={Boolean(errors.email) || Boolean(errors.pass)}
+        >
+          Entrar
+        </button>
+      </div>
+    </form>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
-import Image from "next/image";
+import { FormLogin } from "./components/FormLogin";
 import LayoutContainer from "./components/Layouts";
 
 export default function Home() {
   return (
     <LayoutContainer>
-      <div className="flex flex-col h-96 justify-center">
-        <p className="font-mono text-black text-2xl font-bold mb-3">Login:</p>
-        <input
-          placeholder="e-mail"
-          className="w-64 h-14 rounded-sm bg-white shadow-md"
-        ></input>
-        <button className="w-56 h-14 rounded-xl bg-black shadow-md hover:bg-slate-500">
-          Entrar
-        </button>
-      </div>
+      <FormLogin />
     </LayoutContainer>
   );
 }
