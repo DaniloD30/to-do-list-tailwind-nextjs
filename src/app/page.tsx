60917025a0d8f32d6cfa68b47506f665bb30045d
Danilo Dias <danilolive30@hotmail.com>
2023-12-15 17:30:03 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { FormLogin } from "./components/FormLogin";
 import LayoutContainer from "./components/Layouts";
-//TODO: Verificar responsividade com o tailwind
+
 export default function Home() {
   return (
     <LayoutContainer>
